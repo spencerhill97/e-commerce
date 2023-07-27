3586f6398f0e1f89a46a7124d20b2a8d2e1eca88
spencerhill97 <spencerahill97@gmail.com>
2023-07-26 21:46:16 -0500

    working on making responsive navbar next

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="sticky w-full top-0 bg-gray-200 px-8 py-4 shadow-sm z-10">
       <div className="flex max-w-[1200px] justify-between">
         <h3>Logo</h3>
-        <ul className="flex gap-x-10 capitalize">
+        <ul className="sm:flex gap-x-10 capitalize hidden">
           {navLinks.map((link) => {
             return (
               <li key={link.id} className="grid place-items-center">
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,46 +5,49 @@ import image from "../assets/plant.webp";
 function Cart() {
   return (
     <section
-      className="flex max-w-[1200px] m-auto flex-wrap gap-0 h-full items-start 
-    justify-between p-8"
+      className="md:max-w-[1200px] max-w-[650px] flex flex-col m-auto md:grid md:grid-cols-[3fr_2fr] md:grid-rows-[max_1fr] 
+      gap-x-5 h-full md:items-start justify-between p-5"
     >
-      <h2 className="w-full h-max text-4xl font-bold pb-8">Shopping Cart</h2>
-      <article className="max-w-[650px] w-full h-full flex flex-col border-t-[1px] border-gray-200">
-        <div className="h-[250px] flex py-8 border-b-[1px] border-gray-200 ps-2">
-          <img src={image} alt="plant" className="object-fit aspect-square" />
-          <div className="flex flex-col w-full h-full justify-between pe-2 ps-8">
-            <div className="flex h-max w-full justify-between items-start">
-              <div className="flex flex-col">
-                <h3 className="">Snake Plant</h3>
-                <p className="font-bold">$39.99</p>
+      <h2 className="w-full h-max text-4xl md:col-span-2 md:row-span-1 font-bold pb-8">
+        Shopping Cart
+      </h2>
+      <article
+        className="max-w-[650px] w-full h-full md:col-span-1 md:row-span-1 flex flex-col border-t-[1px]
+                 border-gray-200"
+      >
+        <div className="flex h-[180px] sm:h-[250px] py-8 border-b-[1px] border-gray-200 sm:ps-2">
+          <img src={image} alt="plant" className="aspect-square" />
+          <div className="flex flex-col w-full h-full justify-between sm:pe-2 ps-4">
+            <div className="flex flex-col h-full w-full justify-between items-start p-2">
+              <div className="flex max-[900px]:flex-col justify-between w-full">
+                <h3 className="font-medium">Snake Plant</h3>
+                <p className="font-medium">$39.99</p>
               </div>
-              <select className="pt-2">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
-              <button className="text-xl text-gray-500 pt-2">
-                <MdClose />
-              </button>
+              <div className="flex justify-between w-full">
+                <p className="text-gray-500">Qty 1</p>
+                <p className="flex items-center gap-x-3 text-red-500 font-bold">
+                  Remove
+                </p>
+              </div>
             </div>
-            <p className="flex items-center gap-x-3">
-              <FcCheckmark className="text-lg" /> In stock
-            </p>
           </div>
         </div>
       </article>
-      <article className="flex flex-col gap-y-4 p-8 w-full max-w-[450px] h-100% bg-gray-50 rounded-md">
+      <article
+        className="flex flex-col place-self-end gap-y-4 mt-8 float-right md:mt-auto p-8 w-full md:col-span-1 md:row-span-1 
+        max-w-[450px] h-100% bg-gray-50 rounded-md"
+      >
         <h3 className="text-xl pb-4 font-medium">Order Summary</h3>
         <div className="w-full flex justify-between border-b-[1px] border-gray-200 pb-4">
           <p className="text-gray-600">Subtotal</p>
           <p className="font-medium">$99.99</p>
         </div>
         <div className="w-full flex justify-between border-b-[1px] border-gray-200 pb-4">
-          <p className="text-gray-600">Shipping Estimate</p>
+          <p className="text-gray-600">Shipping</p>
           <p className="font-medium">$5.00</p>
         </div>
         <div className="w-full flex justify-between border-b-[1px] border-gray-200 pb-4">
-          <p className="text-gray-600">Tax Estimate</p>
+          <p className="text-gray-600">Tax</p>
           <p className="font-medium">$8.33</p>
         </div>
         <div className="w-full flex justify-between pb-4">
